4ccc3a800215ac8b44919ac45a9f121628dd8044
steven-appdev <steventan@outlook.my>
2024-04-11 18:37:29 +0100

    Added tooltip

--- src/components/positive.tsx
+++ src/components/positive.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { IDataFilter, IOption, IPositive, IPositiveResult } from "../interfaces";
+import { IOption, IPositive, IPositiveResult } from "../interfaces";
+import { Tooltip } from "react-tooltip";
 import "../tail.css";
 
 export default function Positive({
@@ -65,27 +66,27 @@ export default function Positive({
    };
 
    return (
-      <div className="flex-1 mx-10 mt-5 mb-6 overflow-scroll overflow-x-hidden border rounded-md">
+      <div className="flex-1 mx-10 mb-6 overflow-scroll overflow-x-hidden border rounded-md">
          <table className="text-md w-full">
             <thead>
                <tr className="bg-slate-300 sticky top-0">
-                  <th className="border-b font-medium text-slate-800 py-3 w-[8%]">
+                  <th className="border-b font-medium text-slate-800 py-3 w-[8%] cursor-default select-none">
                      QID
                   </th>
-                  <th className="border-b font-medium text-slate-800 py-3">
+                  <th className="border-b font-medium text-slate-800 py-3 cursor-default select-none">
                      Question
                   </th>
-                  <th className="border-b font-medium text-slate-800 py-3 w-[10%]">
-                     NSS Benchmark<br></br>(%)
+                  <th className="border-b font-medium text-slate-800 py-3 w-[10%] cursor-default select-none">
+                     <p className="benchmark-ttip hover:underline decoration-dashed underline-offset-[6px] decoration-slate-400">NSS Benchmark<br></br>(%)</p>
                   </th>
-                  <th className="border-b font-medium text-slate-800 py-3 w-[10%]">
-                     Positivity Measure<br></br>(%)
+                  <th className="border-b font-medium text-slate-800 py-3 w-[10%] cursor-default select-none">
+                     <p className="positive-ttip hover:underline decoration-dashed underline-offset-[6px] decoration-slate-400">Positivity Measure<br></br>(%)</p>
                   </th>
-                  <th className="border-b font-medium text-slate-800 py-3 w-[10%]">
-                     Times Rank
+                  <th className="border-b font-medium text-slate-800 py-3 w-[10%] cursor-default select-none">
+                     <p className="times-ttip hover:underline decoration-dashed underline-offset-[6px] decoration-slate-400">Times Rank</p>
                   </th>
-                  <th className="border-b font-medium text-slate-800 py-3 w-[10%]">
-                     Times Rank<br></br>(%)
+                  <th className="border-b font-medium text-slate-800 py-3 w-[10%] cursor-default select-none">
+                     <p className="rank-ttip hover:underline decoration-dashed underline-offset-[6px] decoration-slate-400">Times Rank<br></br>(%)</p>
                   </th>
                </tr>
             </thead>
@@ -122,6 +123,19 @@ export default function Positive({
                ))}
             </tbody>
          </table>
+         <Tooltip anchorSelect=".benchmark-ttip" place="top" className="!text-lg !w-[450px] !py-3">
+            A value that is officially calculated by the NSS to compare academic institutional performance against multiple factors, 
+            such as Mode, Subject, Level, Sex, Disability, Age, and Ethnicity.
+         </Tooltip>
+         <Tooltip anchorSelect=".positive-ttip" place="top" className="!text-lg !w-[450px] !py-3">
+            An aggregated value of responses indicating significant level of satisfaction (e.g., students who have responded with “Very Good” and “Good”).
+         </Tooltip>
+         <Tooltip anchorSelect=".times-ttip" place="top" className="!text-lg !w-[450px] !py-3">
+            A value indicating the performance rank among the 131 providers included in the Times Good University Guide.
+         </Tooltip>
+         <Tooltip anchorSelect=".rank-ttip" place="top" className="!text-lg !w-[450px] !py-3">
+            A value indicating the percentage rank among the 131 providers included in the Times Good University Guide. (e.g., 100% indicates best performing whilst 0% indicates worst performance)
+         </Tooltip>
       </div>
    );
 }
--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Tooltip } from "react-tooltip";
 import Options from "../components/options";
 import RespRate from "../components/resprate";
 import Difference from "../components/difference";
@@ -24,30 +25,40 @@ export default function Overview(){
             </div>
             <div className="mt-5 mx-10 h-[340px]">
                 <div className="flex flex-row h-full">
-                <div className="border rounded-md w-[26%] py-[0.6rem] mr-2">
-                    <RespRate 
-                        question={question}
-                        option={option}
-                    />
+                    <div className="border rounded-md w-[26%] py-[0.6rem] mr-2">
+                        <RespRate 
+                            question={question}
+                            option={option}
+                        />
+                    </div>
+                    <div className="border rounded-md w-[37%] pl-5 pr-10 mr-2">
+                        <History 
+                            question={question}
+                            option={option}
+                        />
+                    </div>
+                    <div className="border rounded-md w-[37%] px-5">
+                        <Difference 
+                            question={question}
+                            option={option}
+                        />
+                    </div>
                 </div>
-                <div className="border rounded-md w-[37%] pl-5 pr-10 mr-2">
-                    <History 
-                        question={question}
-                        option={option}
-                    />
-                </div>
-                <div className="border rounded-md w-[37%] px-5">
-                    <Difference 
-                        question={question}
-                        option={option}
-                    />
-                </div>
-                </div>
+            </div>
+            <div className="flex justify-end">
+                <p className="w-fit quartile-ttip mx-10 mt-4 mb-1 text-right text-slate-500 hover:underline decoration-dashed underline-offset-[4px] decoration-slate-400">What does these colour mean?</p>
             </div>
             <Positive
                 option={option}
                 onClick={setQuestion}
             />
+            <Tooltip anchorSelect=".quartile-ttip" place="left" className="!text-lg !w-[450px] !py-3">
+                The colour code indicates the Quartile position:
+                <br/>Q1 (Green) represents best performance
+                <br/>Q2 (Yellow) represents good performance
+                <br/>Q3 (Pink) represents bad performance 
+                <br/>Q4 (Red) represents worst performance 
+            </Tooltip>
         </div>
     )
 }
